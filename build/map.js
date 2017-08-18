4790c08bbf3449b4a2b2f428bd12ee9d8e746ffa
OoSpace <oospace@foxmail.com>
2017-08-18 10:04:13 +0800

    Update map.js

--- build/map.js
+++ build/map.js
@@ -2,7 +2,7 @@
  * Created by oospace on 2016/9/20.
  * 自动配置创建HTML模板文件
  * 需求模块 fs
- * 在需求目录下执行 node fs.js
+ * 在需求目录下执行 node .js
  */
 var fs = require("fs");
 
@@ -10,6 +10,36 @@ var head = "<!--_HEAD_CONTAINER_-->";
 var body = "<Edox><!--_BODY_CONTAINER_--></Edox>";
 var foot = "<!--_FOOT_CONTAINER_-->";
 var other = "<!--_OTHER_CONTAINER_-->";
+
+var headerReplace = [
+        '<meta charset="UTF-8">',
+        '<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, minimal-ui" />',
+        '<meta content="yes" name="apple-mobile-web-app-capable" />',
+        '<meta content="black" name="apple-mobile-web-app-status-bar-style" />',
+        '<meta name="format-detection" content="telephone=no" />',
+        '<meta name="full-screen" content="yes">',
+        '<meta name="browsermode" content="application">',
+        '<meta name="x5-fullscreen" content="true">',
+        '<meta name="x5-page-mode" content="app">',
+        '<meta name="description" content="Edox">',
+        '<meta http-equiv="Pragma" content="no-cache">',
+        '<meta http-equiv="Cache-Control" content="no-cache">',
+        '<meta http-equiv="x-dns-prefetch-control" content="on" />',
+        '<meta http-equiv="Expires" content="0">',
+        '<meta name="keywords" content="javascript,node,libs">',
+        '<link rel="icon" href="favicon.ico" type="image/x-icon" />',
+        '<link rel=dns-prefetch href="" >',
+        '<link rel=dns-prefetch href="" >',
+        '<link href="css/edox.core.css" >',
+    ],
+    footerReplace = [
+        '<script src="js/edox.core.js"></script>'
+    ],
+    bodyReplace = [
+        '<div id="Edox-container">',
+        '</div>'
+    ];
+
 //需要创建的文件及对应标题列表
 var pageList = { //页面列表
     //模块A
@@ -36,13 +66,14 @@ for (var key in pageList) {
         "<html>\n" +
         "<head>\n" +
         "<title>" + pageList[key] + "</title>\n" +
+        headerReplace.join("") + "\n" +
         head + "\n" +
         "</head>\n" +
         "<body>\n" +
         body + "\n" +
-
         "</body>\n" +
-        foot + "\n" +
+        footerReplace.join("") + "\n" +
+        foot +
         other + "\n" +
         "</html>";
     //创建文件
