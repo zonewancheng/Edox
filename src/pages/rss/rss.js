5280d7658cc722fb708c92c9996f7c8c6f67a8bf
zone <oosapce@gmail.com>
2018-03-26 23:05:30 +0800

    rss reader

--- src/pages/rss/rss.js
+++ src/pages/rss/rss.js
@@ -28,28 +28,32 @@
 
             for(var i=0,lg=this.rssSourceList.length;i<lg;i++){
                 var link = this.rssSourceList[i];
-                this.parseRss(link,null)
+                this.parseRss(link,this.concat)
             }
 
             },
+            concat:function (arr) {
+                arr = this.articleList.concat(arr);
+                this.articleList = arr;
+            },
             parseRss: function (link,callback) {
-                var _this = this;
-
 
                 var CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
-
+                var arry = [];
                 var parser = new RSSParser();
                 parser.parseURL(CORS_PROXY + link, function (err, feed) {
-                    console.log(feed.title);
+                    //console.log(feed.title);
                     feed.items.forEach(function (entry) {
-                        console.log(entry)
+                        //console.log(entry)
                         var item  = JSON.parse(JSON.stringify(artile));
                             item.title = entry.title;
                             item.link = entry.link;
                             item.mark = entry.categories;
                             item.author = entry.creator;
-                            _this.articleList.push(item);
-                    })
+                            arry.push(item);
+                    });
+
+                    callback(arry)
                 })
             }
         },
