6422a3130f8f5e9b763d1c4c8a5c7e0b03d2fab9
zone <oosapce@gmail.com>
2018-04-07 13:32:09 +0800

    update

--- build/map.js
+++ build/map.js
@@ -14,7 +14,7 @@ var version = new Date().getTime();
 
 var headerReplace = [
         '<meta charset="UTF-8">',
-        '<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, minimal-ui" />',
+        '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">',
         //'<meta content="yes" name="apple-mobile-web-app-capable" />',
         '<meta content="black" name="apple-mobile-web-app-status-bar-style" />',
         '<meta name="format-detection" content="telephone=no" />',
