c223057f20c34aa6def62cd40ecc5a06154e79aa
zone <oosapce@gmail.com>
2018-12-11 20:33:29 +0800

    登录页面UI

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,3 +1,16 @@
 (function () {
+	let loginForm = new Vue({
+		el: "#mainPage",
+		components: {
+			//'header-comp': header,
+		},
+		data:{
+			siteList: [],
+			title:"上古有大能者 以日为食 以月为饮"
+		},
+		methods: {
+		
+		}
+	});
 
 })()
